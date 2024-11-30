f72336f01342e8eeedbd4fbd8746654187a75166
ASSABBANE-SANAA <assabbanesanaa@gmail.com>
2024-11-30 18:36:20 +0100

    front-components

---
+++ vite-project/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className="nav-logo">Healthcare-Chatbot</div>
+      <ul className="nav-menu">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/annotation">Chatbot</Link></li>
+        <li><Link to="/emotion">Sign in</Link></li>
+        <li className='nav-contact'><Link to="/contact">Sign up</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
